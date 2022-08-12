f6f3b9307b26d21829e9137ced67a3a1a9703fa1
StelicaBulbuc <teo.simi.cristi@gmail.com>
2022-08-12 12:04:03 +0300

    Still in developing phase
    Just more needed functions

--- kilo.c
+++ kilo.c
@@ -9,13 +9,23 @@
 #include <termios.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <sys/ioctl.h>
 #include <ctype.h>
 #include <stdio.h>
 
 /*defines*/
-#define CTRL_KEY(k) ((k) & 0x11)
+/*
+  --->terminate key == Q !!!!!
+ */
+#define CTRL_KEY(k) ((k) & 0x51)
 /*data*/
-struct termios orig_termios;
+
+struct editorConfig{
+    int screenrows;
+    int screencols;
+    struct termios orig_termios;
+};
+struct editorConfig E;
 /*terminal*/
 void die(const char *s){
     write(STDOUT_FILENO, "\x1b[2J", 4);
@@ -29,12 +39,12 @@ void disableRawMode(){
     /*
      -->get the terminal back to normal
      */
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
         die("tcsetattr");
 }
 
 void enableRawMode(){
-    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
     /*
      -->now assure that after at the ending of this program the
         terminal comes back to normal
@@ -44,7 +54,7 @@ void enableRawMode(){
     /*
      -->takes everithing from terminal
      */
-    struct termios raw=orig_termios;
+    struct termios raw=E.orig_termios;
     /*
      -->deactivate all the safety macros, makes the terminal 'RAW'
      */
@@ -67,7 +77,50 @@ char editorReadKey() {
   return c;
 }
 
+int getCursorPosition(int *rows, int *cols){
+    char buf[32];
+    unsigned int i=0;
+    
+    if(write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+    
+    while(i<sizeof(buf)-1){
+        if(read(STDIN_FILENO, &buf[i], 1)!= 1) break;
+        if(buf[i]=='R') break;
+        i++;
+    }
+    buf[i]='\0';
+    
+    if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+    
+    return 0;
+}
+
+int getWindowSize(int *rows, int *cols){
+    struct winsize ws;
+    
+    if( ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws)== -1 || ws.ws_col == 0){
+        if(write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12)!= 12) return -1;
+        editorReadKey();
+        return getCursorPosition(rows, cols);
+    } else {
+        *cols=ws.ws_col;
+        *rows=ws.ws_row;
+        return 0;
+    }
+}
 /*** output ***/
+
+void editorDrawRows(){
+    int y;
+    for(y=0; y<E.screenrows; y++){
+        write(STDOUT_FILENO, "~", 1);
+        
+        if(y<E.screenrows-1){
+            write(STDOUT_FILENO, "\r\n", 2);
+        }
+    }
+}
 
 void editorRefreshScreen() {
   write(STDOUT_FILENO, "\x1b[2J", 4);
@@ -80,7 +133,7 @@ void editorProcessKeypress() {
   char c = editorReadKey();
 
   switch (c) {
-    case CTRL_KEY('q'):
+    case CTRL_KEY('Q'):
       write(STDOUT_FILENO, "\x1b[2J", 4);
       write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
@@ -90,17 +143,18 @@ void editorProcessKeypress() {
 
 /*** init ***/
 
+void initEditor(){
+    if(getWindowSize(&E.screenrows, &E.screencols)==-1) die("getWindowSize");
+}
+
 int main() {
-  enableRawMode();
-  while (1) {
-    char c = '\0';
-    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-    if (iscntrl(c)) {
-      printf("%d\r\n", c);
-    } else {
-      printf("%d ('%c')\r\n", c, c);
+    enableRawMode();
+    initEditor();
+
+    while (1) {
+        editorRefreshScreen();
+        editorProcessKeypress();
     }
-    if (c == CTRL_KEY('y')) break;
-  }
-  return 0;
+    
+    return 0;
 }
---
+++ makefile
@@ -0,0 +1,2 @@
+kiloR: kilo.c	
+	$(CC) kilo.c -o kilo -Wall -Wextra -pedantic -std=c99
