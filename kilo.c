68890a9aba382a510da19bdd3a72a054b2b6127b
StelicaBulbuc <teo.simi.cristi@gmail.com>
2022-08-14 12:33:34 +0300

    a change in emptying the screen
    First important change in reading the line and letting the screen clean. The struct abuf is supposed to work as a dynamic string and the all the functions related as consctructors/destructors.

--- kilo.c
+++ kilo.c
@@ -8,6 +8,7 @@
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
+#include <string.h>
 #include <errno.h>
 #include <sys/ioctl.h>
 #include <ctype.h>
@@ -109,22 +110,55 @@ int getWindowSize(int *rows, int *cols){
         return 0;
     }
 }
+
+/*abbend buffer*/
+
+struct abuf{
+    char *b;
+    int len;
+}
+#define ABUF_INIT{NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int len){
+    char *new = realloc(ab->b, ab->len + len);
+    
+    if(new==NULL) return;
+    memcpy(&new[ab->len], s, len);
+    ab->b=new;
+    ab->len+=len;
+}
+
+void abFree(struct abuf *ab){
+    free(ab->b);
+}
+
 /*** output ***/
 
-void editorDrawRows(){
+void editorDrawRows(struct abuf *ab){
     int y;
     for(y=0; y<E.screenrows; y++){
-        write(STDOUT_FILENO, "~", 1);
+        abAppend(ab, "~", 1);
         
         if(y<E.screenrows-1){
-            write(STDOUT_FILENO, "\r\n", 2);
+            abAppend(ab, "\r\n", 2);
         }
     }
 }
 
 void editorRefreshScreen() {
-  write(STDOUT_FILENO, "\x1b[2J", 4);
-  write(STDOUT_FILENO, "\x1b[H", 3);
+    struct abuf ab = ABUF_INIT;
+    
+    abAppend(&ab, "\x1b[?25l", 6);
+    abAppend(&ab, "\x1b[2J", 4);
+    abAppend(&ab, "\x1b[H", 3);
+    
+    editorDrawRows(&ab);
+    
+    abAppend(&ab, "\x1b[H", 3);
+    abAppend(&ab, "\x1b[?25h", 6);
+    
+    write(STDOUT_FILENO, ab.b, ab.len);
+    abFree(&ab);
 }
 
 /*** input ***/
